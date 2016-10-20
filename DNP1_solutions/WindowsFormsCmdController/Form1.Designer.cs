3cb41249c9181c790ca0ee9f0ba6cb507a5446cc
jimmi280586 <jimmiandersen9@gmail.com>
2016-10-20 23:29:37 +0200

    cmdController started

---
+++ DNP1_solutions/WindowsFormsCmdController/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace WindowsFormsCmdController
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.shutdownComandsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.shutdownNowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.shutdownOnTimerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cancelShutdownToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateAndShutdownToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartComandsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartNowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateAndRestartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartOnTimerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cancelRestartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.moveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.moveFilesFromSubToParentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.forceMoveFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyFilesFromSubToParrentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.forceCopyFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.forceDeletFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteAllFilesInDirectoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteDirectoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.diskParttitionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startDiskPartitionCmdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.installWebserviceInEclipseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.shutdownComandsToolStripMenuItem,
+            this.restartComandsToolStripMenuItem,
+            this.moveToolStripMenuItem,
+            this.copyToolStripMenuItem,
+            this.deleteFilesToolStripMenuItem,
+            this.diskParttitionToolStripMenuItem,
+            this.installWebserviceInEclipseToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1185, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // shutdownComandsToolStripMenuItem
+            // 
+            this.shutdownComandsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.shutdownNowToolStripMenuItem,
+            this.shutdownOnTimerToolStripMenuItem,
+            this.cancelShutdownToolStripMenuItem,
+            this.updateAndShutdownToolStripMenuItem});
+            this.shutdownComandsToolStripMenuItem.Name = "shutdownComandsToolStripMenuItem";
+            this.shutdownComandsToolStripMenuItem.Size = new System.Drawing.Size(127, 20);
+            this.shutdownComandsToolStripMenuItem.Text = "Shutdown Comands";
+            // 
+            // shutdownNowToolStripMenuItem
+            // 
+            this.shutdownNowToolStripMenuItem.Name = "shutdownNowToolStripMenuItem";
+            this.shutdownNowToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.shutdownNowToolStripMenuItem.Text = "Shutdown now";
+            // 
+            // shutdownOnTimerToolStripMenuItem
+            // 
+            this.shutdownOnTimerToolStripMenuItem.Name = "shutdownOnTimerToolStripMenuItem";
+            this.shutdownOnTimerToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.shutdownOnTimerToolStripMenuItem.Text = "Shutdown on timer";
+            // 
+            // cancelShutdownToolStripMenuItem
+            // 
+            this.cancelShutdownToolStripMenuItem.Name = "cancelShutdownToolStripMenuItem";
+            this.cancelShutdownToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.cancelShutdownToolStripMenuItem.Text = "Cancel Shutdown";
+            // 
+            // updateAndShutdownToolStripMenuItem
+            // 
+            this.updateAndShutdownToolStripMenuItem.Name = "updateAndShutdownToolStripMenuItem";
+            this.updateAndShutdownToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.updateAndShutdownToolStripMenuItem.Text = "Update and Shutdown";
+            // 
+            // restartComandsToolStripMenuItem
+            // 
+            this.restartComandsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.restartNowToolStripMenuItem,
+            this.updateAndRestartToolStripMenuItem,
+            this.restartOnTimerToolStripMenuItem,
+            this.cancelRestartToolStripMenuItem});
+            this.restartComandsToolStripMenuItem.Name = "restartComandsToolStripMenuItem";
+            this.restartComandsToolStripMenuItem.Size = new System.Drawing.Size(109, 20);
+            this.restartComandsToolStripMenuItem.Text = "Restart Comands";
+            // 
+            // restartNowToolStripMenuItem
+            // 
+            this.restartNowToolStripMenuItem.Name = "restartNowToolStripMenuItem";
+            this.restartNowToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.restartNowToolStripMenuItem.Text = "Restart now";
+            // 
+            // updateAndRestartToolStripMenuItem
+            // 
+            this.updateAndRestartToolStripMenuItem.Name = "updateAndRestartToolStripMenuItem";
+            this.updateAndRestartToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.updateAndRestartToolStripMenuItem.Text = "Update and Restart";
+            // 
+            // restartOnTimerToolStripMenuItem
+            // 
+            this.restartOnTimerToolStripMenuItem.Name = "restartOnTimerToolStripMenuItem";
+            this.restartOnTimerToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.restartOnTimerToolStripMenuItem.Text = "Restart on Timer";
+            // 
+            // cancelRestartToolStripMenuItem
+            // 
+            this.cancelRestartToolStripMenuItem.Name = "cancelRestartToolStripMenuItem";
+            this.cancelRestartToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.cancelRestartToolStripMenuItem.Text = "Cancel Restart";
+            // 
+            // moveToolStripMenuItem
+            // 
+            this.moveToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.moveFilesFromSubToParentToolStripMenuItem,
+            this.forceMoveFilesToolStripMenuItem});
+            this.moveToolStripMenuItem.Name = "moveToolStripMenuItem";
+            this.moveToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.moveToolStripMenuItem.Text = "Move";
+            // 
+            // moveFilesFromSubToParentToolStripMenuItem
+            // 
+            this.moveFilesFromSubToParentToolStripMenuItem.Name = "moveFilesFromSubToParentToolStripMenuItem";
+            this.moveFilesFromSubToParentToolStripMenuItem.Size = new System.Drawing.Size(230, 22);
+            this.moveFilesFromSubToParentToolStripMenuItem.Text = "Move files from sub to parent";
+            // 
+            // forceMoveFilesToolStripMenuItem
+            // 
+            this.forceMoveFilesToolStripMenuItem.Name = "forceMoveFilesToolStripMenuItem";
+            this.forceMoveFilesToolStripMenuItem.Size = new System.Drawing.Size(230, 22);
+            this.forceMoveFilesToolStripMenuItem.Text = "force move files";
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyFilesFromSubToParrentToolStripMenuItem,
+            this.forceCopyFilesToolStripMenuItem});
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.copyToolStripMenuItem.Text = "Copy";
+            // 
+            // copyFilesFromSubToParrentToolStripMenuItem
+            // 
+            this.copyFilesFromSubToParrentToolStripMenuItem.Name = "copyFilesFromSubToParrentToolStripMenuItem";
+            this.copyFilesFromSubToParrentToolStripMenuItem.Size = new System.Drawing.Size(230, 22);
+            this.copyFilesFromSubToParrentToolStripMenuItem.Text = "copy files from sub to parrent";
+            // 
+            // forceCopyFilesToolStripMenuItem
+            // 
+            this.forceCopyFilesToolStripMenuItem.Name = "forceCopyFilesToolStripMenuItem";
+            this.forceCopyFilesToolStripMenuItem.Size = new System.Drawing.Size(230, 22);
+            this.forceCopyFilesToolStripMenuItem.Text = "force copy files";
+            // 
+            // deleteFilesToolStripMenuItem
+            // 
+            this.deleteFilesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.forceDeletFilesToolStripMenuItem,
+            this.deleteAllFilesInDirectoryToolStripMenuItem,
+            this.deleteDirectoryToolStripMenuItem});
+            this.deleteFilesToolStripMenuItem.Name = "deleteFilesToolStripMenuItem";
+            this.deleteFilesToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.deleteFilesToolStripMenuItem.Text = "Delete files";
+            // 
+            // forceDeletFilesToolStripMenuItem
+            // 
+            this.forceDeletFilesToolStripMenuItem.Name = "forceDeletFilesToolStripMenuItem";
+            this.forceDeletFilesToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.forceDeletFilesToolStripMenuItem.Text = "force delet files";
+            // 
+            // deleteAllFilesInDirectoryToolStripMenuItem
+            // 
+            this.deleteAllFilesInDirectoryToolStripMenuItem.Name = "deleteAllFilesInDirectoryToolStripMenuItem";
+            this.deleteAllFilesInDirectoryToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.deleteAllFilesInDirectoryToolStripMenuItem.Text = "delete all files in directory";
+            // 
+            // deleteDirectoryToolStripMenuItem
+            // 
+            this.deleteDirectoryToolStripMenuItem.Name = "deleteDirectoryToolStripMenuItem";
+            this.deleteDirectoryToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.deleteDirectoryToolStripMenuItem.Text = "delete directory";
+            // 
+            // diskParttitionToolStripMenuItem
+            // 
+            this.diskParttitionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startDiskPartitionCmdToolStripMenuItem});
+            this.diskParttitionToolStripMenuItem.Name = "diskParttitionToolStripMenuItem";
+            this.diskParttitionToolStripMenuItem.Size = new System.Drawing.Size(93, 20);
+            this.diskParttitionToolStripMenuItem.Text = "Disk parttition";
+            // 
+            // startDiskPartitionCmdToolStripMenuItem
+            // 
+            this.startDiskPartitionCmdToolStripMenuItem.Name = "startDiskPartitionCmdToolStripMenuItem";
+            this.startDiskPartitionCmdToolStripMenuItem.Size = new System.Drawing.Size(196, 22);
+            this.startDiskPartitionCmdToolStripMenuItem.Text = "start disk partition cmd";
+            // 
+            // installWebserviceInEclipseToolStripMenuItem
+            // 
+            this.installWebserviceInEclipseToolStripMenuItem.Name = "installWebserviceInEclipseToolStripMenuItem";
+            this.installWebserviceInEclipseToolStripMenuItem.Size = new System.Drawing.Size(163, 20);
+            this.installWebserviceInEclipseToolStripMenuItem.Text = "install webservice in eclipse";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1185, 604);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem shutdownComandsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem shutdownNowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem shutdownOnTimerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cancelShutdownToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateAndShutdownToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartComandsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartNowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateAndRestartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartOnTimerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cancelRestartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem moveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem moveFilesFromSubToParentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem forceMoveFilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyFilesFromSubToParrentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem forceCopyFilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteFilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem forceDeletFilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteAllFilesInDirectoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteDirectoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem diskParttitionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startDiskPartitionCmdToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem installWebserviceInEclipseToolStripMenuItem;
+    }
+}
+
