24c761520aad0097720f13f4cbe919e2154e5024
jimmi280586 <jimmiandersen9@gmail.com>
2016-11-17 10:28:42 +0100

    comments added in auto message

--- CmdController1.0/CmdController1.0/AutoClosingMessageBox.cs
+++ CmdController1.0/CmdController1.0/AutoClosingMessageBox.cs
@@ -12,23 +12,34 @@ namespace CmdController1._0
      */
     public class AutoClosingMessageBox
     {
-        System.Threading.Timer _timeoutTimer;
+        System.Threading.Timer _timeoutTimer;//enitializes a timer thread
         string _caption;
+        //constructor taking in the text, caption is to tell that it have to be uniformed text and then it takes the time in milli seconds
         AutoClosingMessageBox(string text, string caption, int timeout)
         {
+            //set caption to the caption field
             _caption = caption;
+            //instantiate the timer thread
             _timeoutTimer = new System.Threading.Timer(OnTimerElapsed, null, timeout, System.Threading.Timeout.Infinite);
+            //shows the message box using the messagebox method show (system api)
             MessageBox.Show(text, caption);
         }
+        //method to call when wanting to run this timed message box
         public static void Show(string text, string caption, int timeout)
         {
+            //creates a new message box
             new AutoClosingMessageBox(text, caption, timeout);
         }
+        //this method as i understand keeps track of the time the message box is open and when time is up it closes the given box
         void OnTimerElapsed(object state)
         {
+            //find the window with the given caption
             IntPtr mbWnd = FindWindow(null, _caption);
+            //if the time is not zero
             if (mbWnd != IntPtr.Zero)
+                //keep message up if not zero
                 SendMessage(mbWnd, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+            //close the message box
             _timeoutTimer.Dispose();
         }
         /**
