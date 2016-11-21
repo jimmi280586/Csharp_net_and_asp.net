5d4910ace5564428168650c0a7f41a0c7e749ae4
jimmi280586 <jimmiandersen9@gmail.com>
2016-11-21 12:27:38 +0100

    structure changes

---
+++ CmdController1.0/CmdController1.0/CmdComand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmdController1._0
+{
+    class CmdComand
+    {
+    }
+}
---
+++ CmdController1.0/CmdController1.0/CmdLibrary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmdController1._0
+{
+    public class CmdLibrary
+    {
+        private CmdComand[] comands;
+
+        public CmdLibrary()
+        {
+            this.comands = readCmdDatabase();
+        }
+
+        private CmdComand[] readCmdDatabase()
+        {
+            CmdComand[] temp = new CmdComand[3];
+            return temp;
+        }
+    }
+}
